7ec8693c473f42b12282bb7e2c7b0c686b2ace82
Lefteris Mamatas <emamatas@uom.edu.gr>
2023-10-25 10:37:43 +0300

    enabled non-federated selective deployment - v1alpha1

--- cmd/selectivedeployment/main.go
+++ cmd/selectivedeployment/main.go
@@ -23,7 +23,7 @@ import (
 	"strings"
 
 	"github.com/EdgeNet-project/edgenet/pkg/bootstrap"
-	"github.com/EdgeNet-project/edgenet/pkg/controller/apps/v1alpha2/selectivedeployment"
+	"github.com/EdgeNet-project/edgenet/pkg/controller/apps/v1alpha1/selectivedeployment"
 	informers "github.com/EdgeNet-project/edgenet/pkg/generated/informers/externalversions"
 	"github.com/EdgeNet-project/edgenet/pkg/signals"
 
@@ -68,8 +68,8 @@ func main() {
 		kubeInformerFactory.Apps().V1().DaemonSets(),
 		kubeInformerFactory.Apps().V1().StatefulSets(),
 		kubeInformerFactory.Batch().V1().Jobs(),
-		kubeInformerFactory.Batch().V1().CronJobs(),
-		edgenetInformerFactory.Apps().V1alpha2().SelectiveDeployments())
+		kubeInformerFactory.Batch().V1beta1().CronJobs(),
+		edgenetInformerFactory.Apps().V1alpha1().SelectiveDeployments())
 
 	kubeInformerFactory.Start(stopCh)
 	edgenetInformerFactory.Start(stopCh)
